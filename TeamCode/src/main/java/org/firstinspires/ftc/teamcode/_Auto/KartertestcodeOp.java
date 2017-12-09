89fde093ac24bf8cbe2085cb9eff77d1208874db
Jed Lund <jandrew@cpan.org>
2017-12-08 20:02:06 -0800

    New Karter test code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Auto/KartertestcodeOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Auto/KartertestcodeOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode._Auto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode._Libs.AutoLib;
 
@@ -18,82 +19,40 @@ import org.firstinspires.ftc.teamcode._Libs.AutoLib;
 //@Disabled
 public class KartertestcodeOp extends OpMode {
 
-    AutoLib.Sequence mSequence;             // the root of the sequence tree
-    boolean bDone;                          // true when the programmed sequence is done
-    DcMotor mMotors[];                      // motors, some of which can be null: assumed order is fr, br, fl, bl
-    boolean bDebug;
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    boolean bDebug = false;
     //@Override;
     public void init() {
-        AutoLib.HardwareFactory mf = null;
-        final boolean debug = false;
-        if (debug)
-            mf = new AutoLib.TestHardwareFactory(this);
-        else
-            mf = new AutoLib.RealHardwareFactory(this);
-
-        // get the motors: depending on the factory we created above, these may be
-        // either dummy motors that just log data or real ones that drive the hardware
-        // assumed order is fr, br, fl, bl
-        mMotors = new DcMotor[4];/*
-        mMotors[0] = mf.getDcMotor("frontRight");
-        mMotors[1] = mf.getDcMotor("backRight");
-        (mMotors[2] = mf.getDcMotor("frontLeft")).setDirection(DcMotor.Direction.REVERSE);
-        (mMotors[3] = mf.getDcMotor("backLeft")).setDirection(DcMotor.Direction.REVERSE);*/
 
 
         try {
-            mMotors[0] = mf.getDcMotor("frontRight");
-            mMotors[1] = mf.getDcMotor("backRight");
-            (mMotors[2] = mf.getDcMotor("frontLeft")).setDirection(DcMotor.Direction.REVERSE);
-            (mMotors[3] = mf.getDcMotor("backLeft")).setDirection(DcMotor.Direction.REVERSE);
         }
         catch (IllegalArgumentException iax) {
             bDebug = true;
         }
-        // create an autonomous sequence with the steps to drive
-        // several legs of a polygonal course ---
-        float power = 0.1f;
-
-        // create the root Sequence for this autonomous OpMode
-        mSequence = new AutoLib.LinearSequence();
-
-        // add a bunch of timed "legs" to the sequence - use Gyro heading convention of positive degrees CCW from initial heading
-        float leg = debug ? 12.0f : 6.0f;  // time along each leg of the polygon
-
-        // drive a square
-        mSequence.add(new AutoLib.MoveSquirrelyByTimeStep(mMotors, -90, power, leg/2, false));
-        /*
-        mSequence.add(new AutoLib.MoveSquirrelyByTimeStep(mMotors, 0, power, leg, false));
-        mSequence.add(new AutoLib.MoveSquirrelyByTimeStep(mMotors, 90, power, leg, false));
-        mSequence.add(new AutoLib.MoveSquirrelyByTimeStep(mMotors, 180, power, leg, false));
-        mSequence.add(new AutoLib.MoveSquirrelyByTimeStep(mMotors, 270, power, leg/2, false));
-
-        // ... and then a diamond
-        mSequence.add(new AutoLib.MoveSquirrelyByTimeStep(mMotors, -45, power, leg, false));
-        mSequence.add(new AutoLib.MoveSquirrelyByTimeStep(mMotors, 45, power, leg, false));
-        mSequence.add(new AutoLib.MoveSquirrelyByTimeStep(mMotors, 135, power, leg, false));
-        mSequence.add(new AutoLib.MoveSquirrelyByTimeStep(mMotors, 225, power, leg, false));
-
-        // ... and then sort of a polygonal circle
-        int n = 20;     // number of sides
-        for (int i=0; i<n; i++) {
-            double heading = 360*i/n - 90;
-            boolean stop = (i == n-1);
-            mSequence.add(new AutoLib.MoveSquirrelyByTimeStep(mMotors, heading, power, leg/n, stop));
-        }
-*/
-        // start out not-done
         bDone = false;
     }
 
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
     @Override
-    public void loop() {
+    public void init_loop() {
+        if (!bDebug) {
+        }
+    }
 
-        // until we're done, keep looping through the current Step(s)
-        if (!bDone)
-            bDone = mSequence.loop();       // returns true when we're done
-        else
-            telemetry.addData("sequence finished", "");
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    @Override
+    public void loop() {;
     }
 
     @Override
