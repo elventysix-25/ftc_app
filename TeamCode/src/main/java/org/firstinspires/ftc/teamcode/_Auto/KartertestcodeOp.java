e561e4ba7d0d81b21b76b5e6eac700760cce654e
CartAZ <sarkhan1401@gmail.com>
2017-12-15 17:56:07 -0800

    I try new code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Auto/KartertestcodeOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Auto/KartertestcodeOp.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode._Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode._Libs.AutoLib;
@@ -22,16 +24,35 @@ public class KartertestcodeOp extends OpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     boolean bDebug = false;
+    private DcMotor[][] motors = new DcMotor[2][2];
+    // Set default power to 100%
+    private float motorPower = 1.f;
+    private Servo servo = null;
+    private ColorSensor colorSensor;
     //@Override;
     public void init() {
 
 
         try {
+            motors[0][0] = hardwareMap.dcMotor.get("frontLeft");
+            motors[0][1] = hardwareMap.dcMotor.get("frontRight");
+            motors[1][0] = hardwareMap.dcMotor.get("backLeft");
+            motors[1][1] = hardwareMap.dcMotor.get("backRight");
+            servo = hardwareMap.get(Servo.class, "arm");
+            colorSensor=hardwareMap.colorSensor.get("colorSensor");
+            // The motors on the left side of the robot need to be in reverse mode
+            for(DcMotor[] motor : motors){
+                motor[0].setDirection(DcMotor.Direction.REVERSE);
+            }
+            // Being explicit never hurt anyone, right?
+            for(DcMotor[] motor : motors){
+                motor[1].setDirection(DcMotor.Direction.FORWARD);
+            }
         }
         catch (IllegalArgumentException iax) {
             bDebug = true;
         }
-        bDone = false;
+        bDebug = false;
     }
 
     /*
@@ -52,7 +73,10 @@ public class KartertestcodeOp extends OpMode {
     }
 
     @Override
-    public void loop() {;
+    public void loop() {
+        telemetry.addData("Luminosity", colorSensor.alpha());
+        telemetry.addData("Red sensor", colorSensor.red());
+        telemetry.addData("Blue sensor", colorSensor.blue());
     }
 
     @Override
