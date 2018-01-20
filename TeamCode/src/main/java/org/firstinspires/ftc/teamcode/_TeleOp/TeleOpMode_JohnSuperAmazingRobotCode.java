0af18b4955ca716cd8a73536b4967c9de2c2f20b
jnguyen54228 <nguyenduyjohn180@gmail.com>
2018-01-19 19:58:14 -0800

    Fixed mistake in last commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/TeleOpMode_JohnSuperAmazingRobotCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/TeleOpMode_JohnSuperAmazingRobotCode.java
@@ -329,7 +329,6 @@ public class TeleOpMode_JohnSuperAmazingRobotCode extends OpMode
         if (!debugjewelArm) {
             jewelArm.setPosition(.5);
         }
-        }
         if(!debugGripperLeft){
             gripperLeft.setPower(powerGripperLeft);
         }
